5c51fd7abf481fdcfb85a2cdc93e808cd1789a3f
Sztaracsek Matyas <sztaracsekmatyas@gmail.com>
2025-05-27 14:28:58 +0200

    service reteg bealitasa

---
+++ src/main/java/hu/pte/mik/prog4/service/TravelService.java
@@ -0,0 +1,32 @@
+package hu.pte.mik.prog4.service;
+import hu.pte.mik.prog4.entity.TravelEntity;
+import hu.pte.mik.prog4.repository.TravelRepository;
+
+import java.util.List;
+public class TravelService {
+    private final TravelRepository travelRepository;
+
+    public TravelService() {
+        this.travelRepository = new TravelRepository();
+    }
+
+    public List<TravelEntity> listAll() {
+        return this.travelRepository.listAll();
+    }
+
+    public TravelEntity save(TravelEntity travel) {
+        return this.travelRepository.save(travel);
+    }
+
+    public TravelEntity update(TravelEntity travel) {
+        return this.travelRepository.update(travel);
+    }
+
+    public TravelEntity findById(Long id) {
+        return this.travelRepository.findTravelById(id);
+    }
+
+    public Double getRating(String travelId) {
+        return this.travelRepository.getUticel(travelId);
+    }
+}
